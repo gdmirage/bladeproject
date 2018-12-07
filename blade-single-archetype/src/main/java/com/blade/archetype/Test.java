bfe9a9e9e4bee744b68061e1cc72d8fd275a330c
chenjiangxin <chenjiangxin@youkeshu.com.cn>
2018-12-07 10:53:44 +0800

    单个项目的archetype初步搭建

---
+++ blade-single-archetype/src/main/java/com/blade/archetype/Test.java
@@ -0,0 +1,14 @@
+package com.blade.archetype;
+
+/**
+ * TODO:
+ *
+ * @author chenjiangxin
+ * @date 2018/12/7 10:34
+ */
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
