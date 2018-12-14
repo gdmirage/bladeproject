d513d7f0acf2c2b6d682f641e5f5d08b10db94be
chenjiangxin <chenjiangxin@youkeshu.com.cn>
2018-12-14 17:09:40 +0800

    增加查询表名，字段名的测试

--- blade-generator/src/test/com.blade.generator/Test.java
+++ blade-generator/src/test/com.blade.generator/Test.java
@@ -3,12 +3,11 @@ package com.blade.generator;
 import com.blade.generator.util.MysqlConnectionUtil;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -37,6 +36,46 @@ public class Test {
     };
 
     public static void main(String[] args) throws Exception {
+        testDataBase();
+    }
+
+    private static void testDataBase() {
+        try {
+            Connection connection = MysqlConnectionUtil.getConnection();
+            DatabaseMetaData databaseMetaData = connection.getMetaData();
+            ResultSet tables = databaseMetaData.getTables(null,null,"%",null);
+
+            while (tables.next()) {
+
+                String tableName = tables.getString("TABLE_NAME");
+                String remarks = tables.getString("REMARKS");
+
+                System.out.println(tableName + " -- " + remarks);
+
+                ResultSet columnRs = databaseMetaData.getColumns(null, null, tableName, null);
+                while (columnRs.next()) {
+                    String columnName = columnRs.getString("COLUMN_NAME");
+                    String dataType = columnRs.getString("TYPE_NAME");
+                    System.out.println(columnName + " -- " + dataType);
+                }
+
+                System.out.println("-----------------------------------------------");
+            }
+
+//            ResultSet rs = databaseMetaData.getColumns(null, "%", "%", "%");
+//            while (rs.next()) {
+////                System.out.println(rs.getRow());
+//                System.out.println(rs.getString("COLUMN_NAME"));
+//            }
+            if (null != connection) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testThread(){
         ExecutorService service = new ThreadPoolExecutor(
                 corePoolSize, maxPoolSize, keepAliveTime, TimeUnit.MILLISECONDS, workQueue, threadFactory);
 
@@ -48,19 +87,25 @@ public class Test {
         service.shutdown();
     }
 
-    static class Job extends Thread{
+
+    static class Job extends Thread {
         @Override
         public void run() {
-            Connection connection = MysqlConnectionUtil.getConnection();
-            System.out.println(this.getName() +  " " + connection);
-            if(null != connection) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            try {
+                Connection connection = MysqlConnectionUtil.getConnection();
+                DatabaseMetaData databaseMetaData = connection.getMetaData();
+                ResultSet rs = databaseMetaData.getTables(null,"%","%",null);
+                while (rs.next()) {
+                    System.out.println(rs.getRow());
                 }
+                System.out.println(this.getName() + " " + connection);
+                if (null != connection) {
+                    connection.close();
+                }
+                System.out.println(this.getName() + " " + connection);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            System.out.println(this.getName() +  " " + connection);
         }
     }
 }
