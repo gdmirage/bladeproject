fdc74d900b960d38f61845c5f5cf28425d548cbd
Blade <649349299@qq.com>
2019-02-27 21:40:25 +0800

    修改一下配置

--- blade-system/src/main/java/com/blade/manager/system/controller/TestController.java
+++ blade-system/src/main/java/com/blade/manager/system/controller/TestController.java
@@ -45,4 +45,63 @@ public class TestController {
         String version = request.getHeader("interface_version");
         return remoteAddr + " -> " + nginxIp + " -> " + version;
     }
+
+    @GetMapping("/jvm-cpu")
+    public String cpu() {
+        while (true) {
+
+        }
+    }
+
+    @GetMapping("jvm-mem")
+    public String mem() {
+        while (true) {
+            Object obj = new Object();
+        }
+    }
+
+    @GetMapping("jvm-lock")
+    public String lock() {
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    synchronized (DeathLock.left) {
+                        System.out.println(this.getName() + " done left");
+                        synchronized (DeathLock.right) {
+                            System.out.println(this.getName() + " done right");
+                        }
+                    }
+                }
+            }
+        };
+        t1.setName("线程1");
+
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    synchronized (DeathLock.right) {
+                        System.out.println(this.getName() + " done right");
+                        synchronized (DeathLock.left) {
+                            System.out.println(this.getName() + " done left");
+                        }
+                    }
+                }
+            }
+        };
+        ;
+        t2.setName("线程2");
+
+        t1.start();
+        t2.start();
+
+        return "method done";
+    }
+
+}
+
+class DeathLock {
+    public static Object left = new Object();
+    public static Object right = new Object();
 }
