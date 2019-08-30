28b5e81bcb427c483cc99a019a95ac99cd006b45
chenjiangxin <chenjiangxin@cnwansun.com>
2019-08-30 14:12:48 +0800

    增加MongoDB的demo方法

--- blade-practice/src/main/java/com/blade/practice/mongodb/SpringDataMongoDbTest.java
+++ blade-practice/src/main/java/com/blade/practice/mongodb/SpringDataMongoDbTest.java
@@ -1,5 +1,9 @@
 package com.blade.practice.mongodb;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 /**
  * @author blade
  * 2019/8/29 17:33
@@ -7,6 +11,9 @@ package com.blade.practice.mongodb;
 public class SpringDataMongoDbTest {
 
     public static void main(String[] args) {
-        
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/spring-mongo.xml");
+        MongoTemplate mongoTemplate = (MongoTemplate)applicationContext.getBean("mongoTemplate");
+        System.out.println(mongoTemplate.getDb().getName());
+
     }
 }
