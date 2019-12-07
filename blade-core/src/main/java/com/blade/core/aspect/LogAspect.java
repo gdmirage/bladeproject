5d16eb82b7305df9bdcb52b2256802eb7b7f1f4d
chenjiangxin <chenjiangxin@cnwansun.com>
2019-12-07 16:57:54 +0800

    update

--- blade-core/src/main/java/com/blade/core/aspect/LogAspect.java
+++ blade-core/src/main/java/com/blade/core/aspect/LogAspect.java
@@ -3,6 +3,7 @@ package com.blade.core.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,20 +18,39 @@ public class LogAspect {
 
     private Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
-    @Around("execution(* com.blade..controller..*(..))")
-    public Object around(ProceedingJoinPoint joinPoint) {
-        logger.info("around method");
+    @Pointcut("@annotation(com.blade.core.annotation.CalculateExecuteTime)")
+    public void pointCut(){}
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint){
+        logger.info("around method : {}", proceedingJoinPoint);
         long start = System.currentTimeMillis();
         try {
-            Object o = joinPoint.proceed();
+            Object o = proceedingJoinPoint.proceed();
             long end = System.currentTimeMillis();
 
-            logger.info("接口响应为: {} ms", (end - start));
+            logger.info("interface: {},response time is {} ms",proceedingJoinPoint, (end - start));
             return o;
         } catch (Throwable throwable) {
             throwable.printStackTrace();
             return null;
         }
     }
+
+//    @Around("execution(* com.blade..controller..*(..))")
+//    public Object around(ProceedingJoinPoint joinPoint) {
+//        logger.info("around method");
+//        long start = System.currentTimeMillis();
+//        try {
+//            Object o = joinPoint.proceed();
+//            long end = System.currentTimeMillis();
+//
+//            logger.info("接口响应为: {} ms", (end - start));
+//            return o;
+//        } catch (Throwable throwable) {
+//            throwable.printStackTrace();
+//            return null;
+//        }
+//    }
 }
 
