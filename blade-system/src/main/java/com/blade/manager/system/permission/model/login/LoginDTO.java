74a66eb512db9170be99ed1ce482f134626c28ba
chenjiangxin <chenjiangxin@cnwansun.com>
2019-12-20 18:14:53 +0800

    登陆模块编写

---
+++ blade-system/src/main/java/com/blade/manager/system/permission/model/login/LoginDTO.java
@@ -0,0 +1,19 @@
+package com.blade.manager.system.permission.model.login;
+
+import com.blade.core.model.base.JsonAble;
+
+/**
+ * 登陆查询的信息
+ *
+ * @author blade
+ * 2019/12/20 18:05
+ */
+public class LoginDTO extends JsonAble{
+
+    private static final long serialVersionUID = -8351130694224236108L;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ blade-system/src/main/java/com/blade/manager/system/permission/model/login/LoginVO.java
@@ -0,0 +1,19 @@
+package com.blade.manager.system.permission.model.login;
+
+import com.blade.core.model.base.JsonAble;
+
+/**
+ * 登陆成功后的对象信息
+ *
+ * @author blade
+ * 2019/12/20 18:04
+ */
+public class LoginVO extends JsonAble {
+
+    private static final long serialVersionUID = -4924414989596568306L;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
