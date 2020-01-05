bc1cab4ce35e7a270144e115c027742ef67b3c13
blade <649349299@qq.com>
2020-01-05 22:53:46 +0800

    增加netty包

---
+++ blade-practice/src/main/java/com/blade/practice/netty/NettyClient.java
@@ -0,0 +1,10 @@
+package com.blade.practice.netty;
+
+/**
+ * TODO:
+ * netty客户端
+ * @author blade
+ * 2020/1/5 22:49
+ */
+public class NettyClient {
+}
---
+++ blade-practice/src/main/java/com/blade/practice/netty/NettyServer.java
@@ -0,0 +1,14 @@
+package com.blade.practice.netty;
+
+/**
+ * TODO:
+ * netty 服务端
+ * @author blade
+ * 2020/1/5 22:49
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+
+    }
+}
